0670f35af840d46202cc97a5e4fc5e2ed2776db2
Kelvin <kelvinjay1000@gmail.com>
2022-12-01 12:44:19 +0000

    Footer editted

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,10 +7,10 @@ function Footer() {
 		<footer className="p-4 bg-pink rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-pink-800">
 			<span className=" text-white sm:text-center dark:text-white-400">
 				© 2022{" "}
-				<a href="https://flowbite.com/" className="hover:underline">
-					Flowbite™
+				<a href="#" className="hover:underline mr-4">
+					Mon Associates™
 				</a>
-				. All Rights Reserved.
+				All Rights Reserved.
 			</span>
 			<ul className="flex flex-wrap items-center mt-3  text-white dark:text-white-400 sm:mt-0">
 				<li>
@@ -23,11 +23,7 @@ function Footer() {
 						Privacy Policy
 					</a>
 				</li>
-				<li>
-					<a href="#" className="mr-4 hover:underline md:mr-6">
-						Licensing
-					</a>
-				</li>
+			
 				<li>
 					<a href="#" className="hover:underline">
 						Contact
